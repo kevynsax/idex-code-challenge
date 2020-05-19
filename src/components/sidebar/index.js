6151012cd43ff00202cf6c8df5bece15931e2015
Kevyn Klava <kevynsax@gmail.com>
2020-05-19 19:10:58 -0300

    Create router

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,10 +1,30 @@
 import React,{Component} from "react";
+import Logo from '../../app_logo.png';
+import './sidebar.scss';
+
 
 export default class Sidebar extends Component{
-    render = () => (
-        <div>
-            
-        </div>
+    render = () => {
+        const menus = [
+            {label: "Profile", route: "/profile"},
+            {label: "Help", route: "/contactUs"}
+        ];
         
+        return (
+            <div className="sidebar">
+                <div className="logo">
+                    <img src={Logo} alt="IDEX"/>
+                </div>
+                <div className="menus">
+                    {menus.map(this.renderMenu)}
+                </div>
+            </div>
+        );
+    };
+    
+    renderMenu = (menu, index) => (
+        <div key={index} className="menu" onClick={() => document.location.assign(menu.route)}>
+            {menu.label}
+        </div>
     )
 }
