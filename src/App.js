73248954a40cba2c8cd5b12e36019809c75d124f
Kevyn Klava <kevynsax@gmail.com>
2020-05-19 19:19:20 -0300

    Create router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
 import Sidebar from "./components/sidebar";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import Profile from "./components/profile";
+import ContactUs from "./components/contact-us";
 
 function App() {
-  return (
-    <div className="App">
-      <Sidebar />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Sidebar/>
+                <Switch>
+                    <Route path="/profile">
+                        <Profile/>
+                    </Route>
+                    <Route path="/contactUs">
+                        <ContactUs/>
+                    </Route>
+                    <Route path="/" exact={true}>
+                        <Profile/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/contact-us/index.js
@@ -0,0 +1,7 @@
+import React,{Component} from "react";
+
+export default class ContactUs extends Component{
+    render = () => (
+        <div>Contact is here!</div>
+    )
+}
